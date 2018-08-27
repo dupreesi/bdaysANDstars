5d66710137bb7ed9b664d9a783c134533ef7fa47
Simon Dupree <dupreenator@gmail.com>
2018-08-27 19:01:49 +0100

    test build script

---
+++ src/database/test_build.sql
@@ -0,0 +1,53 @@
+BEGIN;
+
+    DROP
+    IF EXISTS users, boards, bdays
+    CASCADE;
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    passw VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users
+    (user_name, passw)
+VALUES
+    ('Armand', 'password12'),
+    ('Simon', 'password12'),
+    ('Eve', 'password12'),
+    ('LikeMike', 'password12');
+
+CREATE TABLE boards
+(
+    id SERIAL PRIMARY KEY,
+    board_name VARCHAR(100),
+    user_id INTEGER REFERENCES users(id)
+);
+
+INSERT INTO boards
+    (board_name, user_id)
+VALUES
+    ('sgcGO122', 1),
+    ('a590bd', 2),
+    ('lkx89sdd', 3),
+    ('b42sdwe', 4);
+
+CREATE TABLE bdays
+(
+    id SERIAL PRIMARY KEY,
+    board_id INTEGER REFERENCES boards(id),
+    name VARCHAR(100),
+    dob DATE,
+    starsign VARCHAR(100),
+    horoscope TEXT
+);
+
+INSERT INTO bdays
+    (board_id, name, dob, starsign, horoscope)
+VALUES
+    (1, 'Simon Dupree', 1987-04-14, 'aries', 'some text bla bla bla'),
+    (3, 'Bernd Dupree', 1947-11-11, 'scorpion', 'more text blablalbla');
+
+COMMIT;
